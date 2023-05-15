d49c81161f569bda27ffc57b18d74bc260aa0738
broyettore <broyettore@gmail.com>
2023-05-15 16:31:39 +0200

    Project Scaffolding

---
+++ database/migrations/2023_05_15_125850_create_games_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string("title", 50);
+            $table->text("description");
+            $table->text("url");
+            $table->float("price", 5, 2);
+            $table->text("genres")->nullable();
+            $table->text("languages")->nullable();
+            $table->string("editor", 50);
+            $table->text("developer");
+            $table->date("release");
+            $table->string("pegi");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
---
+++ database/seeders/GameSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Game;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+class GameSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i < 50; $i++) {
+            $newGame = new Game();
+
+            $newGame->title = $faker->sentence(3);
+            $newGame->description = $faker->text();
+            $newGame->url = $faker->imageUrl(640, 480, 'animals', true);
+            $newGame->price = $faker->randomFloat(2, 1, 80);
+            $newGame->genres = Arr::join($faker->randomElements(["action", "adventure", "arcade", "RPG", "Simulation"], $faker->numberBetween(1, 5)), ",");
+            $newGame->languages = Arr::join($faker->randomElements(["italian", "english", "french", "german", "spanish"], $faker->numberBetween(1, 5)), ",");
+            $newGame->editor = $faker->company();
+            $newGame->developer = $this->generateDev($faker, $faker->randomDigitNot(0));
+            $newGame->release = $faker->dateTime();
+            $newGame->pegi = $faker->randomElement(["3","7","16","12","18"]);
+            $newGame->save();
+        }
+    }
+
+    
+    private function generateDev(Faker $faker, int $n_persons): string{
+
+        $result = [];
+
+        for($i = 0; $i < $n_persons; $i++){
+
+            $person = "{$faker->firstName()} {$faker->lastName()}";
+
+            array_push($result, $person); //['nome cognome', 'nome1 cognome1'];
+
+        }
+
+        //trasformo array in stringa separata da virgola
+        return Arr::join($result, ', ');
+    }
+
+}
