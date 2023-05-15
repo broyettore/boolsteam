d573c924c599e9558c6f817fa413f89649e33acb
broyettore <broyettore@gmail.com>
2023-05-15 16:46:14 +0200

    GameController movde into Admin folder

--- app/Http/Controllers/Admin/GameController.php
+++ app/Http/Controllers/Admin/GameController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Game;
 use App\Http\Requests\StoreGameRequest;
 use App\Http\Requests\UpdateGameRequest;
